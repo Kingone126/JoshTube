27c54fa1d9cdc8c89adc52292cadccef6d077de7
Kingone <127891858+Kingone126@users.noreply.github.com>
2023-04-15 06:33:30 -1200

    Edited the src folder
    created a new src folder

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { Box } from '@mui/material'; 
+
+const App = () => ( 
+    <BrowserRouter>
+
+    </BrowserRouter>
+);
+
+export default App
